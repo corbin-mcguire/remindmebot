195d302e14f6d5a7272e9f171cd52e6c9c28f9de
Corbin McGuire <mcguire.corbin@gmail.com>
2022-12-12 17:51:28 -0500

    Added method to write reminder to file

--- src/reminder.js
+++ src/reminder.js
@@ -1,4 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+
+export let FILE_LOCKED = false;
 
 export class Reminder {
   constructor(owner, date, message) {
@@ -9,8 +12,47 @@ export class Reminder {
   }
 }
 
-// TODO: writes a new reminder to a file
-export function add(reminder) {}
+export function add(reminder) {
+  if (FILE_LOCKED) return;
+
+  const directoryUrl = new URL("../data", import.meta.url);
+  const fileUrl = new URL(`../data/reminders.json`, import.meta.url);
+
+  if (!fs.existsSync(directoryUrl)) {
+    FILE_LOCKED = true;
+    fs.mkdirSync(directoryUrl);
+
+    const remindersJson = {
+      reminders: [reminder],
+    };
+
+    fs.writeFile(fileUrl, JSON.stringify(remindersJson), (err) => {
+      if (err) console.error("Error writing to file", err);
+      FILE_LOCKED = false;
+    });
+
+    FILE_LOCKED = false;
+  } else {
+    fs.readFile(fileUrl, (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      FILE_LOCKED = true;
+
+      const remindersJson = JSON.parse(data);
+      remindersJson.reminders.push(reminder);
+
+      fs.writeFile(fileUrl, JSON.stringify(remindersJson), (err) => {
+        if (err) console.error("Error appending file", err);
+        FILE_LOCKED = false;
+      });
+
+      FILE_LOCKED = false;
+    });
+  }
+}
 
 // TODO: removes a reminder from a file
 export function remove(reminderId) {}
