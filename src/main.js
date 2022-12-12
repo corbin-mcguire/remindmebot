c5254a30a5b3397b59753367d5b3bee31ff6d67d
Corbin McGuire <mcguire.corbin@gmail.com>
2022-12-11 21:44:53 -0500

    Added slash command functionality

--- src/main.js
+++ src/main.js
@@ -1,22 +1,59 @@
-const { Client, GatewayIntentBits } = require("discord.js");
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
-});
+import { Client, GatewayIntentBits, REST, Routes, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import { config } from "dotenv";
+
+config();
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.login(process.env.TOKEN);
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("remindme")
+    .setDescription("Reminds you after a specified amount of time!")
+    .addNumberOption((option) => option.setName("time").setDescription("The time value").setRequired(true))
+    .addStringOption((option) =>
+      option
+        .setName("unit")
+        .setDescription("The unit of time")
+        .setRequired(true)
+        .addChoices(
+          { name: "seconds", value: "seconds" },
+          { name: "minutes", value: "minutes" },
+          { name: "hours", value: "hours" },
+          { name: "days", value: "days" }
+        )
+    )
+    .addStringOption((option) =>
+      option.setName("message").setDescription("The message you'd like to remind yourself of").setRequired(true)
+    ),
+];
+
+async function main() {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("messageCreate", async (message) => {
-  if (message.author.bot) return;
-  if (message.content.startsWith("!remindme".toLowerCase())) {
-    console.log(message.content);
+client.on("interactionCreate", async (interaction) => {
+  console.log(interaction.user.tag, " used /", interaction.commandName);
 
-    message.reply("I'll remind you!");
-  }
-  if (message.content.startsWith("!help")) {
-    message.reply('`!remindme 30(s|m|h) "something to remind you about"`');
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === "remindme") {
+    console.log(interaction.options);
+    await interaction.reply("pong");
   }
 });
-
-client.login("MTA1MTU0Nzg2NTI3NDY1NDc5MQ.GED-ns.FVcoXhsDTU12JO0Jx5F8KdKLCNuuJKbFA5owUw");
